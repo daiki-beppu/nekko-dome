30c4d9684f7bb360aba21c3b02295f6e7212588a
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-15 11:28:01 +0900

    🐞 fix(assets): ファイルパスの変更
    ファイルパスがきちんと設定されていない問題を修正

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import restart from 'vite-plugin-restart';
 
 export default {
   root: 'src/', // Sources files (typically where index.html is)
-  publicDir: 'assets/', // Path from "root" to static assets (files that are served as they are)
+  publicDir: '../assets/', // Path from "root" to static assets (files that are served as they are)
   server: {
     host: true, // Open to local network and display URL
     open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
