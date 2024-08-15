8d36e691b9fb4e352c09ea2602b3507d9683478e
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-15 09:27:09 +0900

    change:
    カメラの設定を変更

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -1,5 +1,6 @@
 import { PerspectiveCamera } from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { createDomeConfig } from './World/Dome/domeConfig';
 
 export class Camera {
   constructor(experience) {
@@ -7,14 +8,14 @@ export class Camera {
     this.sizes = this.experinece.sizes;
     this.scene = this.experinece.scene;
     this.canvas = this.experinece.canvas;
-
     this.cameraConfig = {
       fov: 35,
       aspect: this.sizes.width / this.sizes.height,
       near: 0.1,
       far: 100,
-      position: { x: 6 , y: 4, z: 10 },
+      position: { x: 14, y: 18, z: 32 },
     };
+    this.domeConfig = createDomeConfig();
 
     this.setInstance();
     this.setControls();
@@ -23,6 +24,9 @@ export class Camera {
     const { fov, aspect, near, far, position } = this.cameraConfig;
     this.instance = new PerspectiveCamera(fov, aspect, near, far);
     this.instance.position.set(position.x, position.y, position.z);
+
+    const tragetPosition = this.domeConfig.mesh.position;
+    this.instance.lookAt(tragetPosition.x, tragetPosition.y, tragetPosition.z);
     this.scene.add(this.instance);
   }
   setControls() {
