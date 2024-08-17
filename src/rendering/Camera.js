0c8cee9098083ce86c3bdf4b1964d1fec27e848b
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-17 19:57:51 +0900

    ✨ feat: debugUIの追加と値の調整

--- src/rendering/Camera.js
+++ src/rendering/Camera.js
@@ -7,22 +7,30 @@ export class Camera {
     this.sizes = this.appCore.sizes;
     this.scene = this.appCore.scene;
     this.canvas = this.appCore.canvas;
+    this.debug = this.appCore.debug;
 
     this.cameraConfig = {
       fov: 35,
       aspect: this.sizes.width / this.sizes.height,
       near: 0.1,
       far: 100,
-      position: { x: 4, y: 6, z: 10 },
+      position: { x: 22, y: 18, z: -24 },
     };
+
+    if (this.debug.active) {
+      this.floder = this.debug.ui.addFolder('Camera');
+    }
 
     this.setInstance();
     this.setControls();
+    this.setGUI();
   }
   setInstance() {
     const { fov, aspect, near, far, position } = this.cameraConfig;
     this.instance = new PerspectiveCamera(fov, aspect, near, far);
-    this.instance.position.set(position.x, position.y, position.z);
+    this.instance.position.y = position.y;
+    this.instance.position.x = position.x;
+    this.instance.position.z = position.z;
     this.scene.add(this.instance);
   }
   setControls() {
@@ -37,5 +45,30 @@ export class Camera {
 
   update() {
     this.controls.update();
+  }
+
+  setGUI() {
+    const debugConfig = {
+      positionAxes: ['x', 'y', 'z'],
+      value: { min: -30, max: 30, step: 0.001 },
+    };
+
+    const { min, max, step } = debugConfig.value;
+
+    debugConfig.positionAxes.map((axis) => {
+      this.floder
+        .add(this.instance.position, axis)
+        .min(min)
+        .max(max)
+        .step(step)
+        .name(`position${axis}`)
+        .listen();
+    });
+
+    console.log('Camera position', {
+      x: this.instance.position.x.toFixed(2),
+      y: this.instance.position.y.toFixed(2),
+      z: this.instance.position.z.toFixed(2),
+    });
   }
 }
